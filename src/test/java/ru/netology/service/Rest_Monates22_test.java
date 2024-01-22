4dce154013d29089127deb03266c92b276970b04
Name Surname <vladislavabasarina8@gmail.com>
2024-01-22 17:13:25 +0300

    change commit

--- src/test/java/ru/netology/service/Rest_Monates22_test.java
+++ src/test/java/ru/netology/service/Rest_Monates22_test.java
@@ -3,23 +3,24 @@ package ru.netology.service;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-    class RestServiceTest {
-        @Test
-        public void testCalculate1() {
-            Rest_Monates22 service = new Rest_Monates22();
+class RestServiceTest {
+    @Test
+    public void testCalculate1() {
+        Rest_Monates22 service = new Rest_Monates22();
 
-            int actual = service.calculate (10_000, 3000, 20_000);
-            int expected = 3;
+        int actual = service.calculate(10_000, 3000, 20_000);
+        int expected = 3;
 
-            Assertions.assertEquals(expected,actual);
-        }
+        Assertions.assertEquals(expected, actual);
+    }
 
-        @Test
-        public void testCalculate2() {
-            Rest_Monates22 service = new Rest_Monates22();
+    @Test
+    public void testCalculate2() {
+        Rest_Monates22 service = new Rest_Monates22();
 
-            int actual = service.calculate(100_000, 60_000, 150_000);
-            int expected = 2;
+        int actual = service.calculate(100_000, 60_000, 150_000);
+        int expected = 2;
 
-            Assertions.assertEquals(expected,actual);
-        }}
+        Assertions.assertEquals(expected, actual);
+    }
+}
